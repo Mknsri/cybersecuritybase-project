823a437c1776cabd3d1a4771e1ded4dce49e2654
Markus Mikonsaari <markus.mikonsaari@gmail.com>
2017-01-22 13:31:46 +0000

    Fix for vulnerability nr 5: Leaking password information through cookies OWASP Top 10 type: A6 - Sensitive Data Exposure

--- src/main/java/sec/project/controller/UltraSecure.java
+++ src/main/java/sec/project/controller/UltraSecure.java
@@ -1,20 +1,21 @@
 package sec.project.controller;
 
+import sec.project.config.SecurityConfiguration;
 import sec.project.domain.Account;
 
 public class UltraSecure {
     
     // Awesome function 
-    public static String getSessionToken(String password) {
-        String token = "";
-        for (char c : password.toCharArray()) {
-            token += String.valueOf((int) c);
-        }
+    public static String getSessionToken(String password) {                
+        String token = 
+                new SecurityConfiguration().passwordEncoder().encode(password);
         
         return token;
     }
     
     public static boolean tokenBelongsToAccount(String token, Account acc) {
-        return token.equals(getSessionToken(acc.getPassword()));
+        return new SecurityConfiguration()
+                .passwordEncoder()
+                .matches(acc.getPassword(), token);
     }
 }
